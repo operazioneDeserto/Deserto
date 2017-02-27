54e4b08313c78210d6529b5a215a2987415d613d
Giacomo Camposampiero <giacomo.camposampiero@hotmail.com>
2017-02-27 09:46:24 +0100

    Preincremento variabili.

--- src/agostiniCamposampiero/deserto/deserto/Deserto.java
+++ src/agostiniCamposampiero/deserto/deserto/Deserto.java
@@ -1,105 +1,69 @@
 package agostiniCamposampiero.deserto.deserto;
 
+import agostiniCamposampiero.deserto.carri.*;
 import agostiniCamposampiero.deserto.grafica.*;
 import agostiniCamposampiero.deserto.carri.normali.*;
-import agostiniCamposampiero.deserto.carri.speciali.CarroTalpa;
+import agostiniCamposampiero.deserto.carri.speciali.*;
 import agostiniCamposampiero.deserto.pos.Posizione;
 import agostiniCamposampiero.deserto.strategy.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author giaco
  */
 public class Deserto {
+    
+    private ArrayList<CarroCantiere> armata;
+    private static int difficulty;
 
+    /**
+     * Costruttore non parametrico
+     * Il costruttore costruisce la finestra iniziale
+     */
     public Deserto() {
+        Start start = new Start();
+        start.setVisible(true);
+        start.setSize(295, 280);
     }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        int n1=0, n2=0, n3=0;
-        CarroQuadrato x = new CarroQuadrato(new Posizione(12,22), 9);
-        Strategy stra = new EasyStrategy(50, 60);
-        int stato=0;
-        Posizione pos = stra.nextHit();
-        while(stato>=0){
-            stato = x.fuoco(pos);
-            stra.hitFeedback(stato);
-            pos = stra.nextHit();
-            n1++;
-        }
-        
-        x = new CarroQuadrato(new Posizione(12,22), 9);
-        stra = new MediumStrategy(50, 60);
-        stato=0;
-        pos = stra.nextHit();
-        while(stato>=0){
-            stato = x.fuoco(pos);
-            stra.hitFeedback(stato);
-            pos = stra.nextHit();
-            n2++;
-        }
-        
-        
-        x = new CarroQuadrato(new Posizione(12,22), 9);
-        stra = new DifficultStrategy(50, 60);
-        stato=0;
-        pos = stra.nextHit();
-        while(stato>=0){
-            stato = x.fuoco(pos);
-            stra.hitFeedback(stato);
-            pos = stra.nextHit();
-            n3++;
-        }
-        
-        System.out.println("Facile: "+n1);
-        System.out.println("Media: "+n2);
-        System.out.println("Difficile: "+n3);
-
-         
-
-        /*
-        CarroLineare y = new CarroLineare(new Posizione(1,2), 4);
-        y.fuoco(new Posizione(2,2));
-        System.out.println(y);
-        y.fuoco(new Posizione(2,2));
-        System.out.println(y);
-        y.fuoco(new Posizione(2,2));
-        System.out.println(y);
-        CarroQuadrato z = new CarroQuadrato(new Posizione(1,2), 4);
-        z.fuoco(new Posizione(2,2));
-        System.out.println(z);
-        z.fuoco(new Posizione(2,2));
-        System.out.println(z);
-        z.fuoco(new Posizione(2,2));
-        System.out.println(z);
-        JFrame my = new Start();
-        my.setSize(300,245);
-        my.setVisible(true);
-        */
-        playSound(true);
-        Scanner in = new Scanner(System.in);
-        int i = in.nextInt();
-        playSound(false);
-        i = in.nextInt();
-    }
-
-    private static void playSound(boolean type) {
-        String url;
-        if (type) url = "src/agostiniCamposampiero/deserto/resources/hit.wav";
-        else url = "src/agostiniCamposampiero/deserto/resources/missed.wav";
+        new Deserto();
+    }//main
+    
+    /**
+     * Avvia il simulatore di battaglia
+     */
+    private static void starSimulator(){
+        Grafica x = new Grafica();
+        x.setVisible(true);
+    }//starSimulator
+    
+    /**
+     * Riproduce un effetto sonoro specificato
+     * @param type identificatore dell'effetto sonoro da riprodurre
+     */
+    private void playSound(boolean type) {
+        String url = type?"src/agostiniCamposampiero/deserto/resources/hit.wav":"src/agostiniCamposampiero/deserto/resources/missed.wav";
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(url).getAbsoluteFile());
             Clip clip = AudioSystem.getClip();
@@ -108,5 +72,15 @@ public class Deserto {
         } catch (IOException | LineUnavailableException | UnsupportedAudioFileException e) {
             System.out.println("Errore nel riprodurre l'effetto audio!");
         }
-    }
-}
+    }//playSound
+
+    /**
+     * Setta la difficoltà ed avvia il simulatore
+     * @param x difficoltà
+     */
+    public static void setDifficulty(int x){
+        difficulty = x;
+        starSimulator();
+    }//setDifficulty
+
+}//Deserto
