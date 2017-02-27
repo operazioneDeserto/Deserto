abca0b740879a4720146fdddaceb9d001c2f3250
Giacomo Camposampiero <giacomo.camposampiero@hotmail.com>
2017-02-27 14:40:25 +0100

    Nuova grafica.

--- src/agostiniCamposampiero/deserto/deserto/Deserto.java
+++ src/agostiniCamposampiero/deserto/deserto/Deserto.java
@@ -1,23 +1,28 @@
 package agostiniCamposampiero.deserto.deserto;
 
 import agostiniCamposampiero.deserto.carri.*;
-import agostiniCamposampiero.deserto.grafica.*;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /**
  *
  * @author giaco
  */
-public class Deserto {
+public class Deserto extends JFrame {
     
+    private final JFrame pannelloCtrl;
+    private final JFrame messaggi;
+    private final JLabel backgroundDeserto;
     private ArrayList<CarroCantiere> armata;
     private static int difficulty;
     private final int TIME = 5000;
@@ -28,9 +33,23 @@ public class Deserto {
      * Il costruttore costruisce la finestra iniziale
      */
     public Deserto() {
-        Start start = new Start();
-        start.setVisible(true);
-        start.setSize(295, 280);
+        backgroundDeserto = new javax.swing.JLabel();
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Deserto");
+        getContentPane().setLayout(null);
+        backgroundDeserto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/agostiniCamposampiero/deserto/resources/sand.png"))); // NOI18N
+        getContentPane().add(backgroundDeserto);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int heightGriglia=screenSize.height/10*6, widthGriglia=(int)(screenSize.width/10*7.5);
+        setBounds(20,20,widthGriglia, heightGriglia);
+        setVisible(true);
+        messaggi = new JFrame();
+        pannelloCtrl = new JFrame();
+        messaggi.setBounds(20,heightGriglia+20,(int)(screenSize.width-40), screenSize.height/10*3);
+        messaggi.setVisible(true);
+        pannelloCtrl.setBounds(40+widthGriglia,20,(int)(screenSize.width/10*2)+10, heightGriglia);
+        pannelloCtrl.setVisible(true);
+        backgroundDeserto.setSize(widthGriglia, heightGriglia);
     }//Costruttore
 
     /**
@@ -41,20 +60,10 @@ public class Deserto {
     }//main
     
     /**
-     * Avvia il simulatore di battaglia
-     */
-    private static void startSimulator(){
-        Grafica x = new Grafica();
-        
-
-        playSound(true);        
-    }//starSimulator
-    
-    /**
      * Riproduce un effetto sonoro specificato
      * @param type identificatore dell'effetto sonoro da riprodurre
      */
-    private static void playSound(boolean type) {
+    private void playSound(boolean type) {
         String url = type?"src/agostiniCamposampiero/deserto/resources/hit.wav":"src/agostiniCamposampiero/deserto/resources/missed.wav";
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(url).getAbsoluteFile());
@@ -65,14 +74,5 @@ public class Deserto {
             System.out.println("Errore nel riprodurre l'effetto audio!");
         }
     }//playSound
-
-    /**
-     * Setta la difficoltà ed avvia il simulatore
-     * @param x difficoltà
-     */
-    public static void setDifficulty(int x){
-        difficulty = x;
-        startSimulator();
-    }//setDifficulty
 
 }//Deserto
