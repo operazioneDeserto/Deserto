f6ff4c1cb5f7d0d9fe33e804fadce1b65f9a94ec
Giacomo Camposampiero <giacomo.camposampiero@hotmail.com>
2017-02-28 16:30:31 +0100

    Nuova grafica.

--- src/agostiniCamposampiero/deserto/deserto/Deserto.java
+++ src/agostiniCamposampiero/deserto/deserto/Deserto.java
@@ -2,54 +2,59 @@ package agostiniCamposampiero.deserto.deserto;
 
 import agostiniCamposampiero.deserto.carri.*;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.Rectangle;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSlider;
+import javax.swing.JTextArea;
+import javax.swing.text.DefaultCaret;
 
 /**
  *
  * @author giaco
  */
-public class Deserto extends JFrame {
+public class Deserto extends JFrame implements ActionListener {
     
     private final JFrame pannelloCtrl;
     private final JFrame messaggi;
-    private final JLabel backgroundDeserto;
+    private final JTextArea areaMex;
     private ArrayList<CarroCantiere> armata;
+    private ArrayList<JButton> opzioni;
     private static int difficulty;
     private final int TIME = 5000;
     private int bullets;
+
 
     /**
      * Costruttore non parametrico
      * Il costruttore costruisce la finestra iniziale
      */
     public Deserto() {
-        backgroundDeserto = new javax.swing.JLabel();
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("Deserto");
-        getContentPane().setLayout(null);
-        backgroundDeserto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/agostiniCamposampiero/deserto/resources/sand.png"))); // NOI18N
-        getContentPane().add(backgroundDeserto);
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int heightGriglia=screenSize.height/10*6, widthGriglia=(int)(screenSize.width/10*7.5);
-        setBounds(20,20,widthGriglia, heightGriglia);
-        setVisible(true);
+        areaMex = new JTextArea();
         messaggi = new JFrame();
         pannelloCtrl = new JFrame();
-        messaggi.setBounds(20,heightGriglia+20,(int)(screenSize.width-40), screenSize.height/10*3);
-        messaggi.setVisible(true);
-        pannelloCtrl.setBounds(40+widthGriglia,20,(int)(screenSize.width/10*2)+10, heightGriglia);
-        pannelloCtrl.setVisible(true);
-        backgroundDeserto.setSize(widthGriglia, heightGriglia);
+        frameDeserto();
+        frameMessaggi();
+        frameControllo();
     }//Costruttore
 
     /**
@@ -74,5 +79,58 @@ public class Deserto extends JFrame {
             System.out.println("Errore nel riprodurre l'effetto audio!");
         }
     }//playSound
+    
+    /**
+     * Crea l'interfaccia utente
+     */
+    private void frameDeserto(){
+        JOptionPane.showMessageDialog(this, "Benvenuto nel simulatore di battaglia Deserto.","Deserto",JOptionPane.INFORMATION_MESSAGE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Deserto");
+        getContentPane().setLayout(null);
+        JLabel backgroundDeserto = new JLabel();
+        backgroundDeserto.setIcon(new javax.swing.ImageIcon(getClass().getResource("/agostiniCamposampiero/deserto/resources/sand.png"))); // NOI18N
+        getContentPane().add(backgroundDeserto);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int heightGriglia=screenSize.height/10*6, widthGriglia=(int)(screenSize.width/10*7.5);
+        setBounds(20,20,widthGriglia, heightGriglia);
+        setVisible(true);
+        setResizable(false);
+        backgroundDeserto.setSize(widthGriglia, heightGriglia); 
+        messaggi.setBounds(20,heightGriglia+40,(int)(screenSize.width-40), screenSize.height/10*3);
+        pannelloCtrl.setBounds(40+widthGriglia,20,(int)(screenSize.width/10*2)+10, heightGriglia);
+    }
+    
+    private void frameMessaggi(){
+        messaggi.setTitle("Messaggi dal simulatore");
+        areaMex.setEditable(false);
+        DefaultCaret caret = (DefaultCaret)areaMex.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+        messaggi.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        JScrollPane scroll = new JScrollPane (areaMex, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        messaggi.add(scroll);
+        messaggi.setVisible(true);      
+    }
+    
+    private void frameControllo(){
+        pannelloCtrl.setVisible(true);
+        pannelloCtrl.setResizable(false);
+        pannelloCtrl.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        pannelloCtrl.setTitle("Pannello");
+        JPanel controllo = new JPanel();
+        controllo.setLayout(new GridLayout(8,1));
+        opzioni = new ArrayList<>();
+        for(int i=0; i<8; i++) opzioni.add(new JButton());
+        for(JButton tmp : opzioni){
+            tmp.addActionListener(this);
+            controllo.add(tmp);
+        }
+        pannelloCtrl.add(controllo);
+    }
+        
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        areaMex.append("\n"+"cazzo");
+    }
 
 }//Deserto
